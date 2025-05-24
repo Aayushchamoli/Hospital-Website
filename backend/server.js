3e9cc19eeddfc87c0707686c0c55b5b002c430a0
Aayushchamoli <chamoli2004@gmail.com>
2025-05-24 15:35:35 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from  'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
+
+
+
+//app config
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// app.use('/api/admin',adminRouter)
+//localhost:4000/api/admin
+
+//MIDDLEWARES 
+
+app.use(express.json())
+//app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+// api endpoints 
+
+app.use('/api/admin',adminRouter)
+app.use('/api/doctor',doctorRouter)
+app.use('/api/user',userRouter) 
+
+//app.use('/api/admin',adminRouter)
+//localhost:4000/api/admin
+
+
+
+app.get('/',(req,res)=>{
+    res.send('API WORKING')
+})
+
+app.listen(port, ()=> console.log("Server started",port))
